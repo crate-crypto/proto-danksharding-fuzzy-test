a53db3b30efc9ea46f6670f5170312f0be21d7de
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-11-17 16:16:26 +0000

    add blob_commit test

--- go.mod
+++ go.mod
@@ -2,11 +2,11 @@ module github.com/crate-crypto/proto-danksharding-fuzz
 
 go 1.17
 
-replace github.com/ethereum/go-ethereum => github.com/mdehoog/go-ethereum v1.10.19-0.20221115170900-127584459da6
+replace github.com/ethereum/go-ethereum => github.com/mdehoog/go-ethereum v1.10.19-0.20221117142754-381b51eccf71
 
 require (
 	github.com/consensys/gnark-crypto v0.8.0
-	github.com/crate-crypto/go-proto-danksharding-crypto v0.0.0-20221116175733-e7bd305ca204
+	github.com/crate-crypto/go-proto-danksharding-crypto v0.0.0-20221117160200-27e5f33f2392
 	github.com/ethereum/go-ethereum v0.0.0-00010101000000-000000000000
 	github.com/protolambda/go-kzg v0.0.0-20221108193918-c6d0faa55038
 
@@ -20,7 +20,6 @@ require (
 	github.com/holiman/uint256 v1.2.0 // indirect
 	github.com/kilic/bls12-381 v0.1.1-0.20220929213557-ca162e8a70f4 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
-	github.com/protolambda/go-kzg v0.0.0-20221108193918-c6d0faa55038 // indirect
 	github.com/protolambda/ztyp v0.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/sys v0.2.0 // indirect
--- gokzg/gokzg_test.go
+++ gokzg/gokzg_test.go
@@ -1,14 +1,18 @@
 package gokzg_fuzz
 
 import (
+	"bytes"
 	"encoding/hex"
 	"encoding/json"
 	"io/ioutil"
 	"testing"
 
+	"github.com/crate-crypto/proto-danksharding-fuzz/test_vectors/blob_commit"
 	"github.com/crate-crypto/proto-danksharding-fuzz/test_vectors/roots_of_unity"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto/kzg"
 	protobls "github.com/protolambda/go-kzg/bls"
+	// protobls "github.com/protolambda/go-kzg/bls"
 )
 
 func TestRootsOfUnity(t *testing.T) {
@@ -25,7 +29,7 @@ func TestRootsOfUnity(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		result := FromBytesReversed(expRootBytes)
+		result := FrfromBytesReversed(expRootBytes)
 		if !protobls.EqualFr(&kzg.DomainFr[i], &result) {
 			t.Error("domains are not the same ")
 		}
@@ -33,8 +37,34 @@ func TestRootsOfUnity(t *testing.T) {
 
 }
 
+type Blob = types.Blob
+
+func TestBlobCommit(t *testing.T) {
+	// Unmarshall json test vector
+	file, _ := ioutil.ReadFile("../generated/blob_commit.json")
+	data := blob_commit.BlobCommitJson{}
+	_ = json.Unmarshal([]byte(file), &data)
+
+	for i := 0; i < data.NumBlobs; i++ {
+
+		blob := chunkBlob(data.Blobs[i])
+		expected_comm, _ := hex.DecodeString(data.Commitments[i])
+
+		got_comm, ok := kzg.BlobToKZGCommitment(types.Blob(blob))
+		if !ok {
+			panic("could not compute commitment to the blob")
+		}
+
+		if !bytes.Equal(expected_comm, got_comm[:]) {
+			panic("expected commitment does not match computed commitment")
+		}
+
+	}
+
+}
+
 // Bytes are reversed because test vectors are in big endian
-func FromBytesReversed(b []byte) protobls.Fr {
+func FrfromBytesReversed(b []byte) protobls.Fr {
 	s0 := (*[32]byte)(ReverseBytes(b))
 	var fr protobls.Fr
 	ok := protobls.FrFrom32(&fr, *s0)
@@ -42,6 +72,40 @@ func FromBytesReversed(b []byte) protobls.Fr {
 		panic("fr not valid")
 	}
 	return fr
+}
+
+const chunkSize = 32
+const blobSize = 4096
+
+func chunkBlob(blobStr string) [blobSize]types.BLSFieldElement {
+
+	blobBytes, _ := hex.DecodeString(blobStr)
+	if len(blobBytes)%chunkSize != 0 {
+		// Maybe return an error here, and put it in the test vectors
+		panic("length of blob should be a multiple of 32")
+	}
+
+	var chunks [blobSize]types.BLSFieldElement
+	index := 0
+	for {
+		if len(blobBytes) == 0 {
+			break
+		}
+
+		// necessary check to avoid slicing beyond
+		// slice capacity
+		if len(blobBytes) < chunkSize {
+			panic("this only happens if the blobBytes cannot be chopped into equal chunkSize parts")
+		}
+
+		chunk := ReverseBytes(blobBytes[0:chunkSize])
+		chunkArr := (*[chunkSize]byte)(chunk)
+		chunks[index] = types.BLSFieldElement(*chunkArr)
+		blobBytes = blobBytes[chunkSize:]
+		index += 1
+	}
+
+	return chunks
 }
 
 func ReverseBytes(s []byte) []byte {
