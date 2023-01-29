95c9eff81436358a7b03612b074252b0d3bf1bad
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-01-29 16:54:50 +0000

    hardcode the `POLY_DEGREE` and the `SECRET` since they should not change for the insecure setup

--- main.go
+++ main.go
@@ -3,37 +3,38 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"os"
 
 	context "github.com/crate-crypto/go-proto-danksharding-crypto"
 	agg_proof "github.com/crate-crypto/proto-danksharding-fuzz/test_vectors/agg_proof"
 	blob_commit "github.com/crate-crypto/proto-danksharding-fuzz/test_vectors/blob_commit"
+	precompile "github.com/crate-crypto/proto-danksharding-fuzz/test_vectors/precompile"
 	roots_of_unity "github.com/crate-crypto/proto-danksharding-fuzz/test_vectors/roots_of_unity"
-	transcript "github.com/crate-crypto/proto-danksharding-fuzz/test_vectors/transcript"
 	trusted_setup "github.com/crate-crypto/proto-danksharding-fuzz/test_vectors/trusted_setup"
 	verify_kzg_proof "github.com/crate-crypto/proto-danksharding-fuzz/test_vectors/verify_kzg_proof"
 )
 
 // run this file to generate the json test vector files
 
-const POLY_DEGREE = 4096
-const SECRET = 1337
-
 // Name of the directory that the json files will be added to
 const DIR_NAME = "generated"
 
 func main() {
 
-	c := context.NewContextInsecure(POLY_DEGREE, SECRET)
+	c, err := context.NewContext4096Insecure1337()
+	if err != nil {
+		panic(fmt.Sprintf("failed to create context: %v", err))
+	}
 
 	createTestVectorDir()
 
-	saveAsJson(agg_proof.Generate(c, POLY_DEGREE), location("/public_agg_proof.json"))
-	saveAsJson(transcript.Generate(POLY_DEGREE), location("transcript.json"))
-	saveAsJson(blob_commit.Generate(c, POLY_DEGREE), location("public_blob_commit.json"))
-	saveAsJson(trusted_setup.Generate(c, SECRET, POLY_DEGREE), location("trusted_setup_lagrange.json"))
+	saveAsJson(agg_proof.Generate(c), location("public_agg_proof.json"))
+	saveAsJson(blob_commit.Generate(c), location("public_blob_commit.json"))
+	saveAsJson(trusted_setup.Generate(c), location("trusted_setup_lagrange.json"))
 	saveAsJson(roots_of_unity.Generate(c), location("roots_of_unity.json"))
-	saveAsJson(verify_kzg_proof.Generate(c, POLY_DEGREE), location("public_verify_kzg_proof.json"))
+	saveAsJson(verify_kzg_proof.Generate(c, 2, "verify_kzg_proof"), location("public_verify_kzg_proof.json"))
+	saveAsJson(precompile.Generate(c), location("public_precompile.json"))
 }
 
 func saveAsJson(data interface{}, fileName string) {
